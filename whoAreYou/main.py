a1de816e82b0b2ca5c4ff290a73901f89d3cfb75
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-09-06 10:46:31 -0600

    made code

---
+++ whoAreYou/main.py
@@ -0,0 +1,9 @@
+#Lizzy Saldana, Who are you?
+
+c = input("whats your name?: ")
+b = input("how old are you?:")
+a = input("where do you live?: ")
+
+print("name:",c)
+print("age:", b)
+print("living quarters:", c)
