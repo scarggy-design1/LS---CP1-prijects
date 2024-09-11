0bb91724f0f6aa8641852fc73b2d15bac2af22f1
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-09-11 10:28:06 -0600

    redid

--- tables/main.py
+++ tables/main.py
@@ -1,10 +1,16 @@
 
 # Lizzy Saldana setting and reseting variables
 
-num = (((100*2)+32)/12)
-
-num = (((15*1)+99+29+1)/12)
-
-print(num)
-
-
+f = 32
+s = 100
+ag = s*2
+total = s+(ag)+f
+nt = total/12
+#changes
+f = 29
+s = 99
+ag = 15*1
+m= 1
+total = s+(ag)+f+m
+nt = total/12
+print(nt)
