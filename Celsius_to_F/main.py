3ee59f5353095d9110a5643cb41bc739c68a370d
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-09-11 10:18:09 -0600

    profient

--- Celsius_to_F/main.py
+++ Celsius_to_F/main.py
@@ -1,3 +1,5 @@
+#Lizzy Saldanas profiency test 
+
 temp = int(input("What is the temperature in C, that you would like to convert?: "))
 
 print(((temp*9/5)+32), "degrees Farenheit", "and ", temp ,"celsius")
--- tables/main.py
+++ tables/main.py
@@ -1,3 +1,6 @@
+
+# Lizzy Saldana setting and reseting variables
+
 num = (((100*2)+32)/12)
 
 num = (((15*1)+99+29+1)/12)
