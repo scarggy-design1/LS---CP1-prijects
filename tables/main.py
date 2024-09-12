85f7e1e6b46c95aabccd2b7498261c783be47dc8
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-09-12 10:37:23 -0600

    fixed it hopefully

--- tables/main.py
+++ tables/main.py
@@ -4,13 +4,11 @@
 f = 32
 s = 100
 ag = s*2
-total = s+(ag)+f
-nt = total/12
+nt = ((s+(ag)+f)/12)
 #changes
 f = 29
 s = 99
 ag = 15*1
 m= 1
-total = s+(ag)+f+m
-nt = total/12
+nt = ((s+(ag)+f+m)/12)
 print(nt)
