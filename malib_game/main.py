431ee06616b6c179d73daefc5bc5a3a2d9e8881d
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-09-13 10:36:12 -0600

    UNOOO

---
+++ malib_game/main.py
@@ -0,0 +1,10 @@
+#This is Lizzy Saldanas Malib Game!
+
+name = input("Pick a name! ")
+
+noun = input("Now, pick an object: ")
+
+verb = input("pick an action word: ")
+
+print("it was a chilly day.", name, "wakes up to the smell of burning", noun, "! They scurry down the steps to see that their house is", verb, "! So,", name, "runs out and grabs the burning", noun, ", tosses it out, but it keeps setting everything else", verb, ". There is one last thing that", name, "can try, and thats grabbing the", noun, "and running it under some water." )
+
