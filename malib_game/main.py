249e773dbc288d752b2c8369d7aec962e2176318
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-09-17 10:18:22 -0600

    whattttt-ever

--- malib_game/main.py
+++ malib_game/main.py
@@ -1,10 +1,11 @@
 #This is Lizzy Saldanas Malib Game!
 
-name = input("Pick a name! ")
+name = input("Pick a name!: ")
 
 noun = input("Now, pick an object: ")
-
+v2 = input("Pick a place: ")
 verb = input("pick an action word: ")
+saying = input("Write a phrase!: ")
 
-print("it was a chilly day.", name, "wakes up to the smell of burning", noun, "! They scurry down the steps to see that their house is", verb, "! So,", name, "runs out and grabs the burning", noun, ", tosses it out, but it keeps setting everything else", verb, ". There is one last thing that", name, "can try, and thats grabbing the", noun, "and running it under some water." )
+print("it was a chilly day.", name, "wakes up to the smell of burning", noun, "! They scurry down the steps to see that their house is", verb, "! So,", name, "runs out and grabs the burning", noun, ", tosses it out, but it keeps setting everything else", verb, ". There is one last thing that", name, "can try, and thats grabbing the", noun, "and running it to the", v2, ". But! the", v2, "is, too, on fire!", saying)
 
