79c6a2d420c5d14af26c3ecddc97ccd5d9f73272
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-09-18 10:30:39 -0600

    hiya

---
+++ casting/main.py
@@ -0,0 +1,16 @@
+#this is Lizzy saldanas casting assignment
+
+name = input("What is your name?: ")
+age = int(input("how old are you?: "))
+h = int(input("what is your height in meters?: "))
+fn = int(input("what's your favorite number?: "))
+
+print("converting...")
+
+age2 = int(age) #will now convert age to an integer and age2
+fn2 = int(fn) #favorite number is now going to be treated as an integer
+h2 = float(h) #Height will be converted to a float 
+
+print("age: ", age2)
+print("favorite number: ", fn2)
+print("height: ", h2)
