cbdbb6df0b9d8229401f5b87f479ab787576afaf
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-09-23 10:43:18 -0600

    gpa

---
+++ gpa/main.py
@@ -0,0 +1,14 @@
+#GPA avergae calculator Lizzy Saldana
+
+g1 = float(input("Type in your number grade for first period: "))
+g2 = float(input("Type in your number grade for second period: "))
+g3 = float(input("Type in your number grade for third period: "))
+g4 = float(input("Type in your number grade for fourth period: "))
+g5 = float(input("Type in your number grade for fifth period: "))
+g6 = float(input("Type in your number grade for sixth period: "))
+g7 = float(input("Type in your number grade for seventh period: "))
+g8 = float(input("Type in your number grade for eighth period: "))
+
+gpa = (g1+g2+g3+g4+g5+g6+g7+g8)/8
+
+print("your average grade is: ", gpa)
--- palindrome/main.py
+++ palindrome/main.py
@@ -1,37 +1,5 @@
 #This is Lizzy Saldanas attempt at a palindrome
 
 x = input("Pick a word and I'll check if its a palindrome or not!: ")
-y = "mom"
-a = "a"
-b = "dad"
-c = "racecar"
-d= "civic"
-e = "step on no pets"
-f= "borrow or rob"
-g = "radar"
 
-if x == y:
-    print("Yes! its a palindrome!")
-else:
-    if x == a:
-        print("Yes! its a palindrome!")
-    else:
-        if x == b:
-            print("Yes! its a palindrome!")
-        else:
-            if x == c:
-                print("Yes! its a palindrome!")
-            else:
-                if x == d:
-                    print("Yes! its a palindrome!")
-                else:
-                    if x == e:
-                        print("Yes! its a palindrome!")
-                    else:
-                        if x == f:
-                            print("Yes! its a palindrome!")
-                        else:
-                            if x == g:
-                                print("Yes! its a palindrome!")
-                            else:
-                                 print("No this isn't a palindrome.")
+
