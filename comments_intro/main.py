48c73f424dd529d2a591c701b4e66215ed9982d5
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-10-09 10:17:36 -0600

    good soup

---
+++ comments_intro/main.py
@@ -0,0 +1,5 @@
+#This is my code asking for the user for their name.
+
+name = input("Hello user! What is your name?: ") #this prompts the user to enter their name
+
+print("Hello, ", name, "!")#prints the users inputed name.
