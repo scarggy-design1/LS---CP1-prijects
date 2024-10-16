eac733aee09895af5997eaf7369fd22c9b3ee03b
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-10-16 10:19:49 -0600

    real

--- Formatting/main.py
+++ Formatting/main.py
@@ -1,15 +1,19 @@
 #This is Lizzy Saldana's 'What are these numbers'? assignment
 
-prompt = int(input("Pick a number: "))
+prompt = int(input("Pick a number between 1200-1400: "))
 
-txt = "{:.4f}"
-print(txt.format(prompt))
+if prompt >=1200 and prompt<=1400:
+    txt = "{:.4f}"
+    print("Your number is also", txt.format(prompt))
 
-txt = "{:.0%}"
-print(txt.format(prompt))
+    txt = "{:.0%}"
+    print("You number is also", txt.format(prompt),)
 
-txt = "{:,}"
-print(txt.format(prompt))
+    txt = "{:,}"
+    print("You number is also", txt.format(prompt), "thousand")
 
-txt = "{:.2f}"
-print("$", txt.format(prompt))
+    txt = "{:.2f}"
+    print("Your number is also $", txt.format(prompt))
+    prompt
+else:
+    print("Invalid, choose again.")
