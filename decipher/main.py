e590c08870cb966e311d6c69018de40511624778
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-10-21 10:50:33 -0600

    yes

--- decipher/main.py
+++ decipher/main.py
@@ -1,5 +1,5 @@
 #This is Lizzy saldanas deciphering assignment
-alphabet = ["a", "b", "c", "d", "e", "f", ]
-            
+       
 prompt = input("Type a word...: ")
-def cipher(prompt)
+
+def cipher(prompt):
