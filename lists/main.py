62e0c7a66d6ea39ff5888466b85672b0c2f45e2f
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-10-23 10:29:19 -0600

    finished

--- lists/main.py
+++ lists/main.py
@@ -1,5 +1,38 @@
-#List of classes Lizzy Saldana
 
-classes = ["Leadership", "Computer Science", "Chemistry", "Am civ", "Math", "english." ]
+your_list = []
+def add(your_list):
+    addend = input("What would you like to add?: ")
+    your_list.append(addend)
+    print("your list is now: ", your_list)
+    return your_list
 
-print(*classes,sep='\n')
+def remove(your_list):
+    take = input("What would you like to remove?: ")
+    your_list.remove(take)
+    print("your list is now: ", your_list)
+    return your_list
+
+
+while True:
+
+    action = input("""What would you like to do?
+
+        Enter 1 to add item
+
+        Enter 2 to remove item
+
+        Enter 3 to leave the list:\n""")
+
+    if action =="1":
+
+        add(your_list)
+
+    elif action =="2":
+
+        remove(your_list)
+
+    else:
+
+        print("Have a nice day!")
+
+        break
