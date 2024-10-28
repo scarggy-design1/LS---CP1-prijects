c4c5962e861adc3689b066660c5e68f8e0a9c550
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2024-10-28 10:36:15 -0600

    lets gooo

---
+++ forLoop/main.py
@@ -0,0 +1,25 @@
+#this is lizzy saldanas for loop profiency assignment
+
+num = int(input("Please choose a number: "))
+for x in range(1,num+1):
+    print(num, "multiplication table:")
+    print(num,
+          ''' 
+1  |''' ,num*1, '''
+2  |''' ,num*2, '''
+3  |''' ,num*3, '''
+4  |''' ,num*4, '''
+5  |''' ,num*5, '''
+6  |''' ,num*6, '''
+7  |''' ,num*7, '''
+8  |''' ,num*8, '''
+9  |''' ,num*9, '''
+10 |''' ,num*10, '''
+11 |''' ,num*11, '''
+12 |''' ,num*12.
+    )
+    break
+
+
+
+
