5fbd4cc545639fe5dff4e7eab87e6b68f9e26382
scarggy-design1 <elizabeth.saldana@ucas-edu.net>
2024-11-04 10:31:38 -0700

    bruh

--- Multiplication_Table/main.py
+++ Multiplication_Table/main.py
@@ -1,24 +1,7 @@
 #this is lizzy saldanas for Multiplication Table
 
-num = int(input("Please choose a number: "))
-for x in range(1,num+1):
-    print("Loop has happened ", x, " times")
-    print(num, "multiplication table:")
-    print('''
-1  |''' ,num*1, '''
-2  |''' ,num*2, '''
-3  |''' ,num*3, '''
-4  |''' ,num*4, '''
-5  |''' ,num*5, '''
-6  |''' ,num*6, '''
-7  |''' ,num*7, '''
-8  |''' ,num*8, '''
-9  |''' ,num*9, '''
-10 |''' ,num*10, '''
-11 |''' ,num*11, '''
-12 |''' ,num*12
-    )
-
-
-
-
+num = int(input("choose a number: "))
+for x in range(13):
+    print(
+        x, '''|''', num*x
+        )
