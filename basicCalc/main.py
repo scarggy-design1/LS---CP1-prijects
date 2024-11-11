db610af76987d4d7442b6790d62698ba779c87f7
scarggy-design1 <elizabeth.saldana@ucas-edu.net>
2024-11-11 10:22:32 -0700

    help

--- basicCalc/main.py
+++ basicCalc/main.py
@@ -1,16 +1,1 @@
-#this is lizzy saldanas basic calculator
-
-v1 = int(input("Choose a number: "))
-v2 = int(input("Choose another number: "))
-
-print(v1, "divided by", v2, "=", v1/v2)
-print(v2, "divided by", v1, "=", v2/v1)
-print(v1, "times", v2, "=", v1*v2)
-print(v2, "plus", v1, "=", v2+v1)
-print(v1, "minus", v2, "=", v1-v2)
-print(v2, "minus", v1, "=", v2-v1)
-print(v1, "modules", v2, "=", v1%v2)
-print(v2, "modules", v1, "=", v2%v1)
-print(v1, "raised to", v2, "=", v1**v2)
-print(v2, "raised to", v1, "=", v2**v1)
 
--- decipher/main.py
+++ decipher/main.py
@@ -1,5 +1,8 @@
 #This is Lizzy saldanas deciphering assignment
        
-prompt = input("Type a word...: ")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def cipher(prompt):
+
+#prompt = input("Type a word...: ")
+
+#def cipher(prompt):
