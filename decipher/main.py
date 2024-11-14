53ea834bfd536672158f2fb58982aa93b1ac299d
scarggy-design1 <elizabeth.saldana@ucas-edu.net>
2024-11-14 10:21:07 -0700

    what

--- decipher/main.py
+++ decipher/main.py
@@ -1,16 +1,16 @@
 #This is Lizzy saldanas deciphering assignment
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-text = input("Pick a word: ")
 
-def ciper(x):
-    letters = list(x)
-    for x in alphabet:
-        check = ord(x)
-        check=+1
-        done = chr(check)
-        print(done)
+def ciper():
+    text = input("Pick a word: ")
+    string = ''
+    for x in text:
+        check = (ord(x))+1
+        string = string + chr(check)
+
+    print("orginal: ", text)
+    print("code: ", string)
         
 
-ciper(text)
+ciper()
 
 
