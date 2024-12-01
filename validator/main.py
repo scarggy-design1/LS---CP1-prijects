20173213ae54be7c0730f704aec436e1a79fcf1a
Lizzy S <lizzy.saldana@ucas-edu.net>
2024-12-01 20:01:20 +0000

    bruh

--- validator/main.py
+++ validator/main.py
@@ -1,17 +1,16 @@
 #This is Lizzy Saldana's password validator assignment
-x = ["@", "#", "$", "%", "*", "&"]
-num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+import re
+
 def password_checker():
     while True:
         word = input("What is your password?: ")
-        
         if len(word) < 8:
             print("Please increase the length of your password.")
             continue
-        if x in word:
+        if not re.search(r'[!@#$%^&*(),.?"|<>{}:;]', word):
             print("Please include a symbol: @, #, $, %, *, &")
             continue
-        if num in word:
+        if not re.search(r'\d', word):
             print("Please include a number in your password.")
             continue
         print("Your password is secure!")
