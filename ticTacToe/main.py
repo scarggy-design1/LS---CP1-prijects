be3eb46f0c0de43a50b87cf1d53d83c3fc7d8630
scarggy-design1 <elizabeth.saldana@ucas-edu.net>
2024-12-06 10:43:50 -0700

    SOS

--- ticTacToe/main.py
+++ ticTacToe/main.py
@@ -1,45 +1,132 @@
 #This is lizzy Saldana's tic tac toe game
-
+import random
 print("welcome to tic tac toe! You will be X's and the computer is the O.")
 
 
-grid = [[1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]]
+grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
 choice = ["1. top left", "2. top middle", "3. top right", "4. middle left", "5. exact middle", "6. middle right", "7. bottom left", "8. bottom middle", "9. bottom right"]
 
 def place():
-    options = int(input("Where would you like to go?:", choice))
-         
+    global choice
+    print("Where would you like to go?")
+    options = int(input(choice))
     if options == 1:
+        if grid[0] == 'X':
+            return 'taken'
         grid[0] = "X"
         choice.remove[0]
     elif options == 2:
+        if grid[0] == 'X':
+            return 'taken'
         grid[1] = 'X'
         choice.remove[1]
     elif options == 3:
+        if grid[0] == 'X':
+            return 'taken'
         grid[2] = 'X'
         choice.remove[2]
     elif options == 4:
+        if grid[0] == 'X':
+            return 'taken'
         grid[3] = 'X'
         choice.remove[3]
     elif options == 5:
+        if grid[0] == 'X':
+            return 'taken'
         grid[4] = 'X'
         choice.remove[4]
     elif options == 6:
+        if grid[0] == 'X':
+            return 'taken'
         grid[5] = 'X'
         choice.remove[5]
     elif options == 7:
+        if grid[0] == 'X':
+            return 'taken'
         grid[6] = 'X'
         choice.remove[6]
     elif options == 8:
+        if grid[0] == 'X':
+            return 'taken'
         grid[7] = 'X'
         choice.remove[7]
     elif options == 9:
+        if grid[0] == 'X':
+            return 'taken'
         grid[8] = 'X'
         choice.remove[8]
 
 
+def computer():
+    global choice
+    comp_choice = random.randint(1,9)
+    if comp_choice == 1:
+        if grid[0] == 'X' or grid[0] == 'O':
+            return 'taken'
+        grid[0] = "X"
+        choice.remove[0]
 
-place()
+    elif comp_choice == 2:
+        if grid[1] == 'X' or grid[1] == 'O':
+            return 'taken'
+        grid[1] = 'X'
+        choice.remove[1]
+
+    elif choice == 3:
+        if grid[2] == 'X' or grid[2] == 'O':
+            return 'taken'
+        grid[2] = 'X'
+        choice.remove[2]
+
+    elif choice == 4:
+        if grid[3] == 'X' or grid[3] == 'O':
+            return 'taken'
+        grid[3] = 'X'
+        choice.remove[3]
+        
+    elif choice == 5:
+        if grid[4] == 'X' or grid[4] == 'O':
+            return 'taken'
+        grid[4] = 'X'
+        choice.remove[4]
+
+    elif choice == 6:
+        if grid[5] == 'X' or grid[5] == 'O':
+            return 'taken'
+        grid[5] = 'X'
+        choice.remove[5]
+
+    elif choice == 7:
+        if grid[6] == 'X' or grid[6] == 'O':
+            return 'taken'
+        grid[6] = 'X'
+        choice.remove[6]
+
+    elif choice == 8:
+        if grid[7] == 'X' or grid[7] == 'O':
+            return 'taken'
+        grid[7] = 'X'
+        choice.remove[7]
+
+    elif choice == 9:
+        if grid[8] == 'X' or grid[8] == 'O':
+            return 'taken'
+        grid[8] = 'X'
+        choice.remove[8]
+
+        
+
+while True:
+    player_place = place()
+    comp = computer()
+    if player_place == 'taken':
+        print("Already taken, choose another spot.")
+        place()
+    elif player_place == 'quit':
+        break
+    if comp== 'taken':
+        computer()
+
  
