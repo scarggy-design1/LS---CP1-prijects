13a501a61920a5a295d12cab81314dc590de1901
scarggy-design1 <elizabeth.saldana@ucas-edu.net>
2024-12-12 10:40:50 -0700

    IM SICK OF IT ALL

--- ticTacToe/main.py
+++ ticTacToe/main.py
@@ -1,74 +1,73 @@
 #This is lizzy Saldana's tic tac toe game
 import random
 print("welcome to tic tac toe! You will be X's and the computer is the O.")
-x = 0
-y = 1
-z = 3
 
-grid = ["1 "," ", ' ', ' ', ' ', '4 ', ' ', ' ', '5 ']
+
+grid = [[" 1", "2 ", " 3"], 
+        ["4 ", "5 ", "6 "], 
+        [" 7", " 8", " 9"]]
 
 
 choice = ["1. top left", "2. top middle", "3. top right", "4. middle left", "5. exact middle", "6. middle right", "7. bottom left", "8. bottom middle", "9. bottom right"]
 print("choose 10 to quit.")
-
 def place():
     global choice
     print("Where would you like to go?")
     options = int(input(choice))
 
     if options == 1:
-        if grid[0] == 'X':
+        if grid[0][0] == 'X':
             return 'taken'
-        grid[0] = "X"
+        grid[0][0] = "X"
         choice.remove("1. top left")
         
     elif options == 2:
-        if grid[1] == 'X':
+        if grid[0][1] == 'X':
             return 'taken'
-        grid[1] = 'X'
+        grid[0][1] = 'X'
         choice.remove("2. top middle")
 
     elif options == 3:
-        if grid[2] == 'X':
+        if grid[0][2] == 'X':
             return 'taken'
-        grid[2] = 'X'
+        grid[0][2] = 'X'
         choice.remove("3. top right")
 
     elif options == 4:
-        if grid[3] == 'X':
+        if grid[1][0] == 'X':
             return 'taken'
-        grid[3] = 'X'
+        grid[1][0] = 'X'
         choice.remove("4. middle left")
 
 
     elif options == 5:
-        if grid[4] == 'X':
+        if grid[1][1] == 'X':
             return 'taken'
-        grid[4] = 'X'
+        grid[1][1] = 'X'
         choice.remove("5. exact middle")
 
     elif options == 6:
-        if grid[5] == 'X':
+        if grid[1][2] == 'X':
             return 'taken'
-        grid[5] = 'X'
+        grid[1][2] = 'X'
         choice.remove("6. middle right")
 
     elif options == 7:
-        if grid[6] == 'X':
+        if grid[2][0] == 'X':
             return 'taken'
-        grid[6] = 'X'
+        grid[2][0] = 'X'
         choice.remove("7. bottom left")
 
     elif options == 8:
-        if grid[7] == 'X':
+        if grid[2][1] == 'X':
             return 'taken'
-        grid[7] = 'X'
+        grid[2][1] = 'X'
         choice.remove("8. bottom middle")
 
     elif options == 9:
-        if grid[8] == 'X':
+        if grid[2][2] == 'X':
             return 'taken'
-        grid[8] = 'X'
+        grid[2][2] = 'X'
         choice.remove("9. bottom right")
 
     elif options == 10:
@@ -80,57 +79,57 @@ def computer():
     global choice
     comp_choice = random.randint(1,9)
     if comp_choice == 1:
-        if grid[0] == 'X' or grid[0] == 'O':
+        if grid[0][0] == 'X' or grid[0][0] == 'O':
             return 'taken'
-        grid[0] = "O"
+        grid[0][0] = "O"
         choice.remove("1. top left")
 
     elif comp_choice == 2:
-        if grid[1] == 'X' or grid[1] == 'O':
+        if grid[0][1] == 'X' or grid[0][1] == 'O':
             return 'taken'
-        grid[1] = 'O'
+        grid[0][1] = 'O'
         choice.remove("2. top middle")
 
-    elif choice == 3:
-        if grid[2] == 'X' or grid[2] == 'O':
+    elif comp_choice == 3:
+        if grid[0][2] == 'X' or grid[0][2] == 'O':
             return 'taken'
-        grid[2] = 'O'
+        grid[0][2] = 'O'
         choice.remove("3. top right")
 
-    elif choice == 4:
-        if grid[3] == 'X' or grid[3] == 'O':
+    elif comp_choice == 4:
+        if grid[1][0] == 'X' or grid[1][0] == 'O':
             return 'taken'
-        grid[3] = 'O'
+        grid[1][0] = 'O'
         choice.remove("4. middle left")
         
-    elif choice == 5:
-        if grid[4] == 'X' or grid[4] == 'O':
+    elif comp_choice == 5:
+        if grid[1][1] == 'X' or grid[1][1] == 'O':
             return 'taken'
-        grid[4] = 'O'
+        grid[1][1] = 'O'
         choice.remove("5. exact middle")
 
-    elif choice == 6:
-        if grid[5] == 'X' or grid[5] == 'O':
+    elif comp_choice == 6:
+        if grid[1][2] == 'X' or grid[1][2] == 'O':
             return 'taken'
-        grid[5] = 'O'
+        grid[1][2] = 'O'
         choice.remove("6. middle right")
 
-    elif choice == 7:
-        if grid[6] == 'X' or grid[6] == 'O':
+    elif comp_choice == 7:
+        if grid[2][0] == 'X' or grid[2][0] == 'O':
             return 'taken'
-        grid[6] = 'O'
+        grid[2][0] = 'O'
         choice.remove("7. bottom left")
 
-    elif choice == 8:
-        if grid[7] == 'X' or grid[7] == 'O':
+    elif comp_choice == 8:
+        if grid[2][1] == 'X' or grid[2][1] == 'O':
             return 'taken'
-        grid[7] = 'O'
+        grid[2][1] = 'O'
         choice.remove("8. bottom middle")
 
-    elif choice == 9:
-        if grid[8] == 'X' or grid[8] == 'O':
+    elif comp_choice == 9:
+        if grid[2][2] == 'X' or grid[2][2] == 'O':
             return 'taken'
-        grid[8] = 'O'
+        grid[2][2] = 'O'
         choice.remove("9. bottom right")
 
 def outcome():
@@ -141,58 +140,72 @@ def outcome():
         place()
     elif player_place == 10:
         return 'break'
-    computer()
+    
     if comp == 'taken':
         computer()
 
 
-    if grid[0] and grid[1] and grid[2] == 'X':
+    if grid[0][0] == 'X' and grid[0][1] == 'X' and grid[0][2] == 'X':
         print("X's win!")
         return 'break'
-    elif grid[3] and grid[4] and grid[5] == 'X':
+    
+    elif grid[1][0] == 'X' and grid[1][1] == 'X' and grid[1][2] == 'X':
         print("X's win!")
         return 'break'
-    elif grid[6] and grid[7] and grid[8] == 'X':
+    
+    elif grid[2][0] == 'X' and grid[2][1] == 'X' and grid[2][2] == 'X':
         print("X's win!")
         return 'break'
-    elif grid[0] and grid[4] and grid[8] == 'X':
+    
+    elif grid[0][0] == 'X' and grid[1][1] == 'X' and grid[2][2] == 'X':
         print("X's win!")
         return 'break'
-    elif grid[2] and grid[4] and grid[6] == 'X':
+    
+    elif grid[0][2] == 'X' and grid[1][1] == 'X' and grid[2][0] == 'X':
         print("X's win!")
         return 'break'
-    elif grid[0] and grid[3] and grid[6] == 'X':
+    
+    elif grid[0][0] == 'X' and grid[1][0] == 'X' and grid[2][0] == 'X':
         print("X's win!")
         return 'break'
-    elif grid[2] and grid[5] and grid[8] == 'X':
+    
+    elif grid[0][2] == 'X' and grid[0][2] == 'X' and grid[1][2] == 'X':
         print("X's win!")
         return 'break'
-    elif grid[1] and grid[4] and grid[7] == 'X':
+    
+    elif grid[0][1] == 'X' and grid[1][1] == 'X' and grid[2][1] == 'X':
         print("X's win!")
         return 'break'
-
-    elif grid[0] and grid[1] and grid[2] == 'O':
+    
+    elif grid[0][0] == 'O' and grid[0][1] == 'O' and grid[0][2] == 'O':
         print("O's win!")
         return 'break'
-    elif grid[3] and grid[4] and grid[5] == 'O':
+    
+    elif grid[1][0] == 'O' and grid[1][1] == 'O' and grid[1][2] == 'O':
         print("O's win!")
         return 'break'
-    elif grid[6] and grid[7] and grid[8] == 'O':
+    
+    elif grid[2][0] == 'O' and grid[2][1] == 'O' and grid[2][2] == 'O':
         print("O's win!")
         return 'break'
-    elif grid[0] and grid[4] and grid[8] == 'O':
+    
+    elif grid[0][0] == 'O' and grid[1][1] == 'O' and grid[2][2] == 'O':
         print("O's win!")
         return 'break'
-    elif grid[2] and grid[4] and grid[6] == 'O':
+    
+    elif grid[0][2] == 'O' and grid[1][1] == 'O' and grid[2][0] == 'O':
         print("O's win!")
         return 'break'
-    elif grid[0] and grid[3] and grid[6] == 'O':
+    
+    elif grid[0][0] == 'O' and grid[1][0] == 'O' and grid[2][0] == 'O':
         print("O's win!")
         return 'break'
-    elif grid[2] and grid[5] and grid[8] == 'O':
+    
+    elif grid[0][2] == 'O' and grid[0][2] == 'O' and grid[1][2] == 'O':
         print("O's win!")
         return 'break'
-    elif grid[1] and grid[4] and grid[7] == 'O':
+    
+    elif grid[0][1] == 'O' and grid[1][1] == 'O' and grid[2][1] == 'O':
         print("O's win!")
         return 'break'
 
@@ -201,11 +214,10 @@ def outcome():
 
 
 while True:
-    for i in range(1,4):
-        print(grid[x],"""|""", grid[y],"""|""", grid[z])
-        x+=1
-        y+=1
-        z+=1
+    for i in grid:
+        for x in i:
+            print(x, "|", end="")
+        print("\n")
     
     if outcome() == 'break':
         break
